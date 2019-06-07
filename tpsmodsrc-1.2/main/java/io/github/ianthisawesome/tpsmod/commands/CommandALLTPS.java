9257dc526c7a2a72596d60a37dfa211056815df5
Codetoil <ianthisawesomee@gmail.com>
2019-06-07 07:01:50 -0400

    TPSMODSRC-1.2

---
+++ tpsmodsrc-1.2/main/java/io/github/ianthisawesome/tpsmod/commands/CommandALLTPS.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright Codetoil (c) 2019
+ */
+
+package io.github.ianthisawesome.tpsmod.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.ianthisawesome.tpsmod.TpsMod;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+
+public class CommandALLTPS extends CommandBase {
+
+	private final List<String> aliases;
+	
+	public CommandALLTPS()
+	{
+		aliases = new ArrayList<String>();
+        aliases.add("/tpstoall");
+        aliases.add("/alltps");
+	}
+	
+	public int getRequiredPermissionLevel()
+    {
+        return 0;
+    }
+	
+	@Override
+	public String getCommandName() {
+		return "/tpstoall";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender p_71518_1_) {
+		return "message.tpsmod.alltps.help";
+	}
+
+	@Override
+	public void processCommand(ICommandSender p_71515_1_, String[] p_71515_2_) {
+		TpsMod.getTPS.doCommandStuffCommandALLTPS(p_71515_1_, p_71515_2_);
+	}
+
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<String> getCommandAliases() {
+		return this.aliases;
+	}
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender p_71519_1_) {
+		return true;
+	}
+
+	@Override
+	public List<String> addTabCompletionOptions(ICommandSender p_71516_1_, String[] p_71516_2_) {
+		List<String> tabCompletions = new ArrayList<String>();
+		tabCompletions.add("help");
+		tabCompletions.add("?");
+		return tabCompletions;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] p_82358_1_, int p_82358_2_) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
